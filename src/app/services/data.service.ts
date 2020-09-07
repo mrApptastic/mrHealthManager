0f0a0fd1512bf6e1d0f043a53c85eae65aee1e88
mrApptastic <henrikbeske@gmail.com>
2020-09-07 10:45:49 +0200

    Massive changes to data service.

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -18,238 +18,126 @@ export class DataService {
 
   constructor(private http: HttpClient, private cookie: CookieService) { }
 
+  /* Persons */
   getPerson(id: number): Person {
-    return this.getPersons().find(x => x.Id === id);
+    return this.getSingleData('persons', id) as Person;
   }
 
   getPersons(): Person[] {
-    return this.getData('persons');
+    return this.getData('persons') as Person[];
   }
 
   setPerson(person: Person): Person {
-    const personList = this.getPersons();
-    if (personList && personList.some(x => x.Id === person.Id)) {
-      const per =  personList.find(x => x.Id === person.Id);
-      for (const ll in person) {
-        per[ll] = person[ll];
-      }
-      // per.Name = person.Name;
-      // per.Height = person.Height;
-      // per.Weight = person.Weight;
-      // per.DateOfBirth = person.DateOfBirth;
-      // per.Gender = person.Gender;
-      // per.StrideLength = person.StrideLength;
-      // if (!person.Activities) {
-      //   per.Activities = new Array();
-      // } else {
-      //   per.Activities = person.Activities;
-      // }
-      // if (!person.Consumption) {
-      //   per.Consumption = new Array();
-      // } else {
-      //   per.Consumption = person.Consumption;
-      // }
-      // if (!person.History) {
-      //   per.History = new Array();
-      // } else {
-      //   per.History = person.History;
-      // }
-      // if (!person.Plans) {
-      //   per.Plans = new Array();
-      // } else {
-      //   per.Plans = person.Plans;
-      // }
-    } else if (personList) {
-      if (person.Id === 0) {
-        if (personList.length > 0) {
-          person.Id = personList.sort((x, y) => y.Id - x.Id)[0].Id + 1;
-        } else {
-          person.Id = 1;
-        }
-      }
-      personList.push(person);
-    }
-    this.setPersons(personList);
-
-    return person;
+    return this.setSingleData(person, "persons") as Person;
   }
 
-  setPersons(persons: Person[]): void {
-    const dataObj = this.getDataObject();
-    dataObj.persons = persons;
-    this.setDataObject(dataObj);
+  setPersons(persons: Person[]): Person[] {
+    return this.setData(persons, 'persons') as Person[];
   }
 
+  /* Activities */
   getActivitiesFromTemplate(): Observable<Activity[]> {
     return this.http.get<Activity[]>('assets/activities.json');
   }
 
   getActivity(id: number): Activity {
-    return this.getActivities().find(x => x.Id === id);
+    return this.getSingleData('activities', id) as Activity;
   }
 
   getActivities(): Activity[] {
-    return this.getData('activities');
+    return this.getData('activities') as Activity[];
   }
 
   setActivity(activity: Activity): Activity {
-    const activityList = this.getActivities();
-    if (activityList && activityList.some(x => x.Id === activity.Id)) {
-      const act =  activityList.find(x => x.Id === activity.Id);
-      act.kCal = activity.kCal;
-      act.Name = activity.Name;
-      act.UseKmH = activity.UseKmH;
-    } else if (activityList) {
-      if (activity.Id === 0) {
-        if (activityList.length > 0) {
-          activity.Id = activityList.sort((x, y) => y.Id - x.Id)[0].Id + 1;
-        } else {
-          activity.Id = 1;
-        }
-      }
-      activityList.push(activity);
-    }
-    this.setActivities(activityList);
-
-    return activity;
+    return this.setSingleData(activity, "activities") as Activity;
   }
 
-  setActivities(activities: Activity[]): void {
-    const dataObj = this.getDataObject();
-    dataObj.activities = activities;
-    this.setDataObject(dataObj);
+  setActivities(activities: Activity[]): Activity[] {
+    return this.setData(activities, 'activities') as Activity[];
   }
 
+  /* Food Types */
   getFoodTypesFromTemplate(): Observable<FoodType[]> {
     return this.http.get<FoodType[]>('assets/foodTypes.json');
   }
 
+  getFoodType(id: number): FoodType {
+    return this.getSingleData('foodTypes', id) as FoodType;
+  }
+
   getFoodTypes(): FoodType[] {
-    return this.getData('foodTypes');
+    return this.getData('foodTypes') as FoodType[];
   }
 
-  setFoodType(foodType: FoodType): FoodType {
-    const foodTypeList = this.getFoodTypes();
-    if (foodTypeList && foodTypeList.some(x => x.Id === foodType.Id)) {
-      const ft =  foodTypeList.find(x => x.Id === foodType.Id);
-      ft.Type = foodType.Type;
-    } else if (foodTypeList) {
-      if (foodType.Id === 0) {
-        if (foodTypeList.length > 0) {
-          foodType.Id = foodTypeList.sort((x, y) => y.Id - x.Id)[0].Id + 1;
-        } else {
-          foodType.Id = 1;
-        }
-      }
-      foodTypeList.push(foodType);
-    }
-    this.setFoodTypes(foodTypeList);
-
-    return foodType;
+  setFoodType(foodtype: FoodType): FoodType {
+    return this.setSingleData(foodtype, "foodTypes") as FoodType;
   }
 
-  setFoodTypes(foodTypes: FoodType[]): void {
-    const dataObj = this.getDataObject();
-    dataObj.foodTypes = foodTypes;
-    this.setDataObject(dataObj);
+  setFoodTypes(foodtypes: FoodType[]): FoodType[] {
+    return this.setData(foodtypes, 'foodTypes') as FoodType[];
   }
 
+  /* Food */
   getFoodFromTemplate(): Observable<Food[]> {
     return this.http.get<Food[]>('assets/food.json');
   }
 
-  getFood(): Food[] {
-    return this.getData('food');
+  getSingleFood(id: number): Food {
+    return this.getSingleData('foodTypes', id) as Food;
   }
 
+  getFood(): Food[] {
+    return this.getData('foodTypes') as Food[];
+  }
+
+  setSingleFood(food: Food): Food {
+    return this.setSingleData(food, "foodTypes") as Food;
+  }
+
+  setFood(food: Food[]): Food[] {
+    return this.setData(food, 'foodTypes') as Food[];
+  }
+
+  /* Plans */
   getPlan(id: number): Plan {
-    return this.getPlans().find(x => x.Id === id);
+    return this.getSingleData('plans', id) as Plan;
   }
 
   getPlans(): Plan[] {
-    return this.getData('plans');
+    return this.getData('plans') as Plan[];
   }
 
   setPlan(plan: Plan): Plan {
-    const planList = this.getPlans();
-    if (planList && planList.some(x => x.Id === plan.Id)) {
-      const pl =  planList.find(x => x.Id === plan.Id);
-      pl.Length = plan.Length;
-      pl.Goal = plan.Goal;
-      if (!pl.Days) {
-        pl.Days = new Array();
-      } else {
-        pl.Days = pl.Days;
-      }
-    } else if (planList) {
-      if (plan.Id === 0) {
-        if (planList.length > 0) {
-          plan.Id = planList.sort((x, y) => y.Id - x.Id)[0].Id + 1;
-        } else {
-          plan.Id = 1;
-        }
-      }
-      planList.push(plan);
-    }
-    this.setPlans(planList);
-
-    return plan;
+    return this.setSingleData(plan, "plans") as Plan;
   }
 
-  setPlans(plans: Plan[]): void {
-    const dataObj = this.getDataObject();
-    dataObj.plans = plans;
-    this.setDataObject(dataObj);
+  setPlans(plans: Plan[]): Plan[] {
+    return this.setData(plans, 'plans') as Plan[];
   }
 
+  /* Logs */
   getLog(id: number): ErrorLog {
-    return this.getLogs().find(x => x.Id === id);
+    return this.getSingleData('logs', id) as ErrorLog;
   }
 
   getLogs(): ErrorLog[] {
-    return this.getData('logs');
+    return this.getData('logs') as ErrorLog[];
   }
 
   setLog(log: ErrorLog): ErrorLog {
-    const logList = this.getLogs();
-    if (logList && logList.some(x => x.Id === log.Id)) {
-      const l =  logList.find(x => x.Id === log.Id);
-      for (const ll in log) {
-        l[ll] = log[ll];
-      }
-      // l.Time = log.Time;
-      // l.Name = log.Name;
-      // l.Message = log.Message;
-
-      // l.CookiesEnabled = log.CookiesEnabled;
-      // l.Details = log.Details;
-      // l.MaxTouchPoints = log.MaxTouchPoints;
-      // l.Message = log.Message;
-      // l.Platform = log.Platform;
-      // l.WebBrowser = log.WebBrowser;
-    } else if (logList) {
-      if (log.Id === 0) {
-        if (logList.length > 0) {
-          log.Id = logList.sort((x, y) => y.Id - x.Id)[0].Id + 1;
-        } else {
-          log.Id = 1;
-        }
-      }
-      logList.push(log);
-    }
-    this.setLogs(logList);
-
-    return log;
+    return this.setSingleData(log, "logs") as ErrorLog;
   }
 
-  setLogs(logs: ErrorLog[]): void {
-    const dataObj = this.getDataObject();
-    dataObj.logs = logs;
-    this.setDataObject(dataObj);
+  seLogs(logs: ErrorLog[]): ErrorLog[] {
+    return this.setData(logs, 'logs') as ErrorLog[];
   }
 
   dataCheck(): boolean {
     return !!this.cookie.getLocal(this.data);
+  }
+
+  getSingleData(type: string, id: number): any {
+    return this.getData(type).find(x => x.Id === id);
   }
 
   getData(type: string): any {
@@ -261,7 +149,7 @@ export class DataService {
     }
   }
 
-  setData(changes: any, type: string): any {
+  setSingleData(changes: any, type: string): any {
     const dataObj = this.getDataObject();
     const list = dataObj[type];
 
@@ -280,17 +168,19 @@ export class DataService {
       }
       list.push(changes);
     }
-    // save mangler.
-    // this.setLogs(dataObj);
+
+    this.setData(dataObj[type], type);
 
     return changes;
 
   }
 
-  setDatas() {
+  setData(changes: any, type: string): any {
     const dataObj = this.getDataObject();
-    // dataObj.persons = persons;
+    dataObj[type] = changes;
     this.setDataObject(dataObj);
+
+    return changes;
   }
 
   getDataObject(): Data {
