79142e61240faefed844bc1418297ff0166f87c6
mrApptastic <henrikbeske@gmail.com>
2020-11-03 21:11:44 +0100

    Added decimal pipe and exceptions, added delay before table filter and changed project preview settings.

--- src/app/components/basic-table/basic-table.component.ts
+++ src/app/components/basic-table/basic-table.component.ts
@@ -1,24 +1,36 @@
 import { Component, OnInit, OnChanges, Input, Output, EventEmitter } from '@angular/core';
-import { DatePipe } from '@angular/common';
+import { Subject } from "rxjs";
+import { debounceTime } from 'rxjs/operators';
+import { DatePipe, DecimalPipe } from '@angular/common';
 
 @Component({
   selector: 'app-basic-table',
   templateUrl: './basic-table.component.html',
   styleUrls: ['./basic-table.component.scss'],
-  providers: [DatePipe]
+  providers: [DatePipe, DecimalPipe]
 })
 export class BasicTableComponent implements OnInit, OnChanges {
   @Input() dataSource: any[];
   @Input() editable: any;
   @Output() rowClick = new EventEmitter<any>();
   @Output() dataChange = new EventEmitter<any>();
+  searchRequest = new Subject<string>();
   editAll: boolean;
   sortOrder: string;
   sortDirection: boolean;
-  searchFilter: string;
+  searchField: string;
   columns: any[];
+  searchFilter = "";
 
-  constructor(private datePipe: DatePipe) {}
+  constructor(private datePipe: DatePipe,
+              private numberPipe: DecimalPipe) {
+                this.searchRequest
+                .pipe(
+                  debounceTime(500))
+                .subscribe(() => {
+                  this.searchFilter = this.searchField;
+                });
+              }
 
   ngOnInit(): void {
     this.loadData();
@@ -26,6 +38,10 @@ export class BasicTableComponent implements OnInit, OnChanges {
 
   ngOnChanges(): void {
     this.loadData();
+  }
+
+  handleSearch(): void {
+    this.searchRequest.next();
   }
 
   loadData() {
@@ -56,6 +72,14 @@ export class BasicTableComponent implements OnInit, OnChanges {
       returnValue =  this.datePipe.transform(returnValue, "dd/MM yyyy");
     } else if (col.toString().toLowerCase() === "time") {
       returnValue =  this.datePipe.transform(returnValue, "dd/MM yyyy HH:mm");
+    } else if (col.toString().toLowerCase() === "calories" ||
+              col.toString().toLowerCase() === "proteins" ||
+              col.toString().toLowerCase() === "fat" ||
+              col.toString().toLowerCase() === "carbohydrates" ||
+              col.toString().toLowerCase() === "fibres" ||
+              col.toString().toLowerCase() === "calcium" ||
+              col.toString().toLowerCase() === "alcohol") {
+      returnValue =  this.numberPipe.transform(returnValue);
     }
 
     return returnValue;
@@ -73,8 +97,9 @@ export class BasicTableComponent implements OnInit, OnChanges {
             col.width = length;
           }
         } else {
-          if (!Array.isArray(value)) {
-
+          if (!Array.isArray(value) &&
+          (key.toString().toLowerCase() !== "id" &&
+           key.toString().toLowerCase() !== "typeid")) {
             columns.push({
               Name: key,
               Width: key.toString().length > value.toString().length ? key.toString().length : value.toString().length,
