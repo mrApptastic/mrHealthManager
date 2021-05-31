94c276419815225867e252b4dd9c12295d9fb99d
mrApptastic <30831839+mrApptastic@users.noreply.github.com>
2021-05-31 23:52:06 +0200

    Search box component stuff.

--- src/app/components/search-box/search-box.component.ts
+++ src/app/components/search-box/search-box.component.ts
@@ -1,17 +1,35 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, forwardRef, Input, OnInit } from '@angular/core';
+import { NG_VALUE_ACCESSOR } from '@angular/forms';
+import { AbstractInputComponent } from '../abstract-input/abstract-input.component';
+
 
 @Component({
   selector: 'app-search-box',
   templateUrl: './search-box.component.html',
-  styleUrls: ['./search-box.component.scss']
+  styleUrls: ['./search-box.component.scss'],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => SearchBoxComponent),
+      multi: true,
+    },
+  ],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class SearchBoxComponent implements OnInit {
+export class SearchBoxComponent extends AbstractInputComponent implements OnInit {
   @Input("dataSource") dataSource: any[];
-  searchText = "";
+  @Input("inputClass") inputClass: string;
+  @Input("selectClass") selectClass: string;
 
-  constructor() { }
+  cid = Math.floor(Math.random() * Number.MAX_SAFE_INTEGER);
 
   ngOnInit(): void {
-  }
+    if (!this.inputClass) {
+      this.inputClass = "";
+    }
 
+    if (!this.selectClass) {
+      this.selectClass = "";
+    }
+  }
 }
